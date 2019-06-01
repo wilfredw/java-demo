33cd2cde95273423f803741b0e15947441ef7a2a
Wang Wei <wangwei2@myhexin.com>
2019-06-01 11:58:19 +0800

    aa

---
+++ base-demo/src/main/java/com/wei/java/reflect/ReflectDemo.java
@@ -0,0 +1,38 @@
+package com.wei.java.reflect;
+
+/**
+ * Created by viruser on 2019/6/1.
+ */
+public class ReflectDemo {
+    private int x;
+    private int y;
+    private String comment;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void print() {
+        System.out
+    }
+}
